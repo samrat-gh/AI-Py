1ba7a88d204d655b37ab4418936caa95fd4824db
samrat-gh <samratghimire32@gmail.com>
2024-06-27 09:47:15 +0545

    add initial code

---
+++ index.py
@@ -0,0 +1,13 @@
+x = int(1)
+y = float(1.4243)
+z = str("samrat")
+
+a, b, c = "this", "is", "smart"
+print(a + b + c)
+guess = 10
+
+if guess == 10: 
+    print("It is greater")
+
+elif guess < 10:
+    prinf("It is smaller")
